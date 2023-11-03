3f1b2a52a29fbe8156df983b37fb258bb37b0897
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-03 23:21:04 +0100

    field naming telegram_handle

--- PhriniFluentAPI/models.py
+++ PhriniFluentAPI/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    telegram_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    telegram_handle = models.CharField(max_length=100, unique=True, null=True, blank=True)
 
 
 class APIKey(models.Model):
