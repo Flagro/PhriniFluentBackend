ccf51da97ff0652a569dbf6002eddc8b8610bff3
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-04 11:09:48 +0100

    IsAdminUser import

--- PhriniFluentAPI/views.py
+++ PhriniFluentAPI/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import CustomUser, APIKey, Language, WordGroup, Word, WordDescription, UserWordGroup
 from .serializers import (CustomUserSerializer, APIKeySerializer, LanguageSerializer,
