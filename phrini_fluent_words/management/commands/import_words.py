65b861d66c7520755c50eaf325038c32d0cdae7c
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-08 00:39:23 +0100

    updated words initialization script

--- phrini_fluent_words/management/commands/import_words.py
+++ phrini_fluent_words/management/commands/import_words.py
@@ -1,5 +1,5 @@
-from django.core.management.base import BaseCommand, CommandError
-from words.models import Word, WordDescription, WordGroup  # Replace with your actual model names and app name
+from django.core.management.base import BaseCommand
+from phrini_fluent_words.models import Word, WordDescription, WordGroup, WordGroupDescription, Language
 import json
 
 class Command(BaseCommand):
@@ -12,30 +12,32 @@ class Command(BaseCommand):
         with open(options['json_file'], 'r', encoding='utf-8') as file:
             data = json.load(file)
 
-        for language, content in data.items():
-            description_translations = content['description']
-            # Assuming you have a WordGroup model for each language
-            word_group, created = WordGroup.objects.get_or_create(name=language)
-
-            # Update descriptions for each language
-            for lang_code, description in description_translations.items():
-                setattr(word_group, f'description_{lang_code}', description)
-            word_group.save()
+        for language_name, content in data.items():
+            word_group, created = WordGroup.objects.get_or_create(name=language_name)
+            
+            # Assuming you have a Language model instance for each language name
+            for lang_name, description in content['description'].items():
+                language, _ = Language.objects.get_or_create(language_name=lang_name)
+                WordGroupDescription.objects.update_or_create(
+                    word_group=word_group,
+                    language=language,
+                    defaults={'description_text': description}
+                )
 
             # Now add words
             for word_item in content['words']:
                 word, created = Word.objects.get_or_create(
-                    word=word_item['word'],
-                    word_group=word_group,
-                    defaults={'quiz_type': word_item['quiz_type']}
+                    text=word_item['word'],
+                    word_group=word_group
                 )
 
                 # Add descriptions for the word in different languages
-                for lang_code, description in word_item['descriptions'].items():
-                    WordDescription.objects.get_or_create(
+                for lang_name, description in word_item['descriptions'].items():
+                    language, _ = Language.objects.get_or_create(language_name=lang_name)
+                    WordDescription.objects.update_or_create(
                         word=word,
-                        language=lang_code,
-                        defaults={'description': description}
+                        language=language,
+                        defaults={'description_text': description}
                     )
-        
+
         self.stdout.write(self.style.SUCCESS('Successfully imported words'))
