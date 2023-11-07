d5885aa7f436464d56816e7041b3a7e3e7ed6096
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-08 00:16:12 +0100

    new serializers implementation

--- phrini_fluent_words/serializers.py
+++ phrini_fluent_words/serializers.py
@@ -1,30 +1,40 @@
 from rest_framework import serializers
-from .models import Language, WordGroup, Word, WordDescription, UserWordGroup
+from .models import Language, WordGroup, WordGroupDescription, Word, WordDescription
 
 
 class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Language
-        fields = ['id', 'language_tag', 'language_name']
+        fields = ['language_name']
+
+
+class WordGroupDescriptionSerializer(serializers.ModelSerializer):
+    language = LanguageSerializer(read_only=True)
+
+    class Meta:
+        model = WordGroupDescription
+        fields = ['description_text', 'language']
+
 
 class WordGroupSerializer(serializers.ModelSerializer):
+    descriptions = WordGroupDescriptionSerializer(many=True, read_only=True)
+
     class Meta:
         model = WordGroup
-        fields = ['id', 'name', 'language', 'description', 'is_global', 'users']
+        fields = ['id', 'name', 'descriptions']
 
-class WordSerializer(serializers.ModelSerializer):
-    descriptions = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    
-    class Meta:
-        model = Word
-        fields = ['id', 'word_group', 'text', 'language', 'descriptions']
 
 class WordDescriptionSerializer(serializers.ModelSerializer):
+    language = LanguageSerializer(read_only=True)
+
     class Meta:
         model = WordDescription
-        fields = ['id', 'word', 'language', 'description_text']
+        fields = ['description_text', 'language']
 
-class UserWordGroupSerializer(serializers.ModelSerializer):
+
+class WordSerializer(serializers.ModelSerializer):
+    descriptions = WordDescriptionSerializer(many=True, read_only=True)
+
     class Meta:
-        model = UserWordGroup
-        fields = ['id', 'user', 'word_group', 'date_added']
+        model = Word
+        fields = ['id', 'text', 'descriptions']
