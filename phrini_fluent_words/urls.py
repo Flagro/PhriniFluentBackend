863bd399c2dd901698dda9adda6eeedb306ba964
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-08 00:56:50 +0100

    updated urls for words

--- phrini_fluent_words/urls.py
+++ phrini_fluent_words/urls.py
@@ -2,19 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Language URLs
-    path('languages/', views.language_list, name='language-list'),
-    path('languages/<str:language_tag>/', views.language_detail, name='language-detail'),
-    
     # WordGroup URLs
-    path('word-groups/', views.word_group_list, name='word-group-list'),
-    path('word-groups/<int:pk>/', views.word_group_detail, name='word-group-detail'),
-    
-    # Word URLs
-    path('words/', views.word_list, name='word-list'),
-    path('words/<int:pk>/', views.word_detail, name='word-detail'),
-    
-    # WordDescription URLs
-    path('word-descriptions/', views.word_description_list, name='word-description-list'),
-    path('word-descriptions/<int:pk>/', views.word_description_detail, name='word-description-detail'),
+    path('word-groups/public/', views.public_word_group_list, name='public-word-group-list'),
+    path('word-groups/private/', views.private_word_group_list, name='private-word-group-list'),
+    path('word-groups/<int:pk>/random/', views.private_word_group_list, name='random_word_from_group'),
+    path('words/<int:word_id>/similarity/', views.word_similarity, name='word-similarity'),
 ]
