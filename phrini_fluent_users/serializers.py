e4245683bc734d6870d8472e642fb68b26d6acd6
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-08 01:08:31 +0100

    implemented serializers for users

--- phrini_fluent_users/serializers.py
+++ phrini_fluent_users/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
-from .models import PhriniFluentUser
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
 
-class PhriniFluentUserSerializer(serializers.ModelSerializer):
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PhriniFluentUser
-        fields = ['id', 'username', 'telegram_handle', 'email']
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'telegram_handle')
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email', 'telegram_handle')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ('key',)
