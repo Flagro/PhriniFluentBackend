1810a595f24956bb97fe90fe3b30e3e333d629c2
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-14 15:14:34 +0100

    static files and media

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,13 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Nginx will serve from here in production
+
+# Add MEDIA_URL and MEDIA_ROOT if you have media files
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
