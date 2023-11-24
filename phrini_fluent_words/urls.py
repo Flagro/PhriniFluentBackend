1c45153a40de299e824e971bda8ff327e283248e
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-24 17:10:30 +0100

    [HOTFIX] view function for random word

--- phrini_fluent_words/urls.py
+++ phrini_fluent_words/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # WordGroup URLs
     path('word-groups/public/', views.public_word_group_list, name='public-word-group-list'),
     path('word-groups/private/', views.private_word_group_list, name='private-word-group-list'),
-    path('word-groups/<int:pk>/random/', views.private_word_group_list, name='random_word_from_group'),
+    path('word-groups/<int:pk>/random/', views.random_word_from_group, name='random_word_from_group'),
     path('words/<int:word_id>/similarity/', views.word_similarity, name='word-similarity'),
 ]
